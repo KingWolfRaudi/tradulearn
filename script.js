60fe199c96f072154ae09be836d1173e27529dad
Oscar Orta <oeos02@gmail.com>
2026-05-02 02:00:58 -0400

    update fix

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+window.addEventListener('load', function() {
+    const loadOverlay = document.getElementById('load-overlay');
+    if (loadOverlay) {
+        loadOverlay.classList.add('hide');
+    }
+});
+
 function openStartOverlay() {
     const startOverlay = document.getElementById('start-overlay');
     if (startOverlay) {
